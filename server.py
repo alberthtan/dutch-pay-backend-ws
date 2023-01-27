ee1d869a4e066221460802c0b8ce84e1b0d65b3e
Albert Tan <alberttan@Alberts-MacBook-Pro.local>
2023-01-27 14:12:27 -0500

    first commit - added server.py

---
+++ server.py
@@ -0,0 +1,63 @@
+import asyncio
+import websockets
+
+CLIENTS = set()
+NUM_CLIENTS = 0
+MESSAGE_LIST = []
+
+async def handler(websocket):
+    global NUM_CLIENTS
+    global MESSAGE_LIST
+    
+    if websocket not in CLIENTS:
+        # print("adding client " + str(NUM_CLIENTS))
+        # print(websocket)
+        NUM_CLIENTS += 1
+        CLIENTS.add(websocket)
+        if(len(MESSAGE_LIST) != 0):
+            await websocket.send(MESSAGE_LIST[-1])
+            print("sending message to new client")
+
+        # print(CLIENTS)
+    
+    # BROADCAST IS CALLED ONCE PER CLICK
+    async for message in websocket:
+        print("appending new message to message list")
+        MESSAGE_LIST.append(message)
+        await broadcast(message)
+
+    try:
+        await websocket.wait_closed()
+    finally:
+        CLIENTS.remove(websocket)
+
+async def broadcast(message):
+    # i = 0
+    for websocket in CLIENTS.copy():
+        # print(i)
+        # i += 1
+        try:
+            await websocket.send(message)
+        except websockets.ConnectionClosed:
+            pass
+
+# async def broadcast_messages():
+#     while True:
+#         await asyncio.sleep(1)
+#         message = 'hello'
+#         await broadcast(message)
+
+# async def echo(websocket):
+#     async for message in websocket:
+#         # print(type(message))
+#         print(message)
+#         await broadcast()
+#         await websocket.send("yoyoyo")
+#         await websocket.send(message)
+
+# DO NOT BROADCAST IN MAIN
+async def main():
+    async with websockets.serve(handler, "10.0.0.26", 8000):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
