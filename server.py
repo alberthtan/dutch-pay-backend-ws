e0a69b84af87e3e63467bd8f46802a15a281ad4f
Albert Tan <alberttan@Alberts-MacBook-Pro.local>
2023-01-27 15:06:47 -0500

    test

--- server.py
+++ server.py
@@ -60,4 +60,5 @@ async def main():
     async with websockets.serve(handler, "", 8080):
         await asyncio.Future()  # run forever
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
