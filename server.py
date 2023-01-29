49a4b4141d802e26d795a746012e6b1d674f000d
Albert Tan <alberttan@Alberts-MacBook-Pro.local>
2023-01-28 23:56:56 -0500

    cleaning up code

--- server.py
+++ server.py
@@ -20,17 +20,10 @@ async def handler(websocket):
     
         if(len(MESSAGE_LIST) != 0):
             await websocket.send(MESSAGE_LIST[-1])
-    
-    print(CLIENTS)
 
-
-        # print(CLIENTS)
-    
-    # BROADCAST IS CALLED ONCE PER CLICK
+    # PROCESS MESSAGE
     async for message in websocket:
-        print("appending new message to message list")
         print(json.loads(message))
-        print(CLIENTS)
         table_id = json.loads(message)['table_id']
 
         
@@ -41,26 +34,28 @@ async def handler(websocket):
         if not websocket in CLIENT_TABLES[table_id]:
             CLIENT_TABLES[table_id].append(websocket)
             CLIENT_TABLEID_LOOKUP[websocket] = table_id
+
+            # Send cart data if it exists, the first time a user goes on
             if(len(MESSAGE_LIST) != 0):
                 await websocket.send(MESSAGE_LIST[-1])
 
-        # Otherwise treat message as an edit to cart
+        # If user already exists, treat message as an edit to cart
         else:
             MESSAGE_LIST.append(message)
             await broadcast(message)
 
-    # SHOULD RUN IF USER ORDERS
+    # USER PRESSES ORDER
     try:
         await websocket.wait_closed()
     finally:
-        # CLEAN UP
+        # Remove user from table and CLIENTS 
         print("cleaning up")
         table_id = CLIENT_TABLEID_LOOKUP[websocket]
         CLIENT_TABLES[table_id].remove(websocket)
         del CLIENT_TABLEID_LOOKUP[websocket]
         CLIENTS.remove(websocket)
-        print(CLIENTS)
 
+        # If no users at table, remove table
         if not CLIENT_TABLES[table_id]:
             print("clearing table")
             MESSAGE_LIST.clear()
@@ -68,30 +63,12 @@ async def handler(websocket):
             print(CLIENT_TABLES)
 
 async def broadcast(message):
-    # i = 0
     for websocket in CLIENTS.copy():
-        # print(i)
-        # i += 1
         try:
             await websocket.send(message)
         except websockets.ConnectionClosed:
             pass
 
-# async def broadcast_messages():
-#     while True:
-#         await asyncio.sleep(1)
-#         message = 'hello'
-#         await broadcast(message)
-
-# async def echo(websocket):
-#     async for message in websocket:
-#         # print(type(message))
-#         print(message)
-#         await broadcast()
-#         await websocket.send("yoyoyo")
-#         await websocket.send(message)
-
-# DO NOT BROADCAST IN MAIN
 async def main():
     async with websockets.serve(handler, host="", port=os.environ.get('PORT', 8000)):
         await asyncio.Future()  # run forever
