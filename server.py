a663d9d932e878b817ea021073d112b482eb0ae8
Allen Chun <allenchun@Allens-MacBook-Pro.local>
2023-02-22 16:55:09 -0500

    test

--- server.py
+++ server.py
@@ -11,6 +11,9 @@ CLIENT_TABLES = dict() # {table_id: {websocket1, websocket2 ...}}
 CLIENT_TABLEID_LOOKUP = dict()# {websocket: table_id}
 CART_DICT = dict() # {table_id: [cartItem, cartItem, ...]}
 
+SERVER_TABLES = dict() # {table_id: [websocket1, websocket2]}
+SERVER_TABLE_LOOKUP = dict() # {websocket: [table_id, ...]}
+
 async def handler(websocket):
     global NUM_CLIENTS
     # global MESSAGE_LIST
@@ -21,50 +24,80 @@ async def handler(websocket):
             message = json.loads(websocket_message)
             print(message)
 
-            table_id = message['table_id']
-
-
-            # Add user to CLIENT_TABLES if nonexistent
-            if not table_id in CLIENT_TABLES:
-                CLIENT_TABLES[table_id] = []
-            if not websocket in CLIENT_TABLES[table_id]:
-                print("adding websocket to client tables")
-                
-                CLIENT_TABLES[table_id].append(websocket)
-                CLIENT_TABLEID_LOOKUP[websocket] = table_id
-
-                print(CLIENT_TABLES)
-
-            # Send latest cart data if user goes to Menu screen from camera screen
+            # Message from a customer
             if 'flag' in message:
-                if table_id in CART_DICT:
-                    json_message = json.dumps(list(CART_DICT[table_id].values()), default=lambda o: o.__dict__, indent=4)
-                    await websocket.send(json_message)
-                    
-            # All other messages should be treated as edits to the cart
-            else:
-                if not table_id in CART_DICT:
-                    CART_DICT[table_id] = dict()
-                if message['action'] == 'add':
-                    cartItem = CartItem(message['id'], message['item'], message['user'])
-                    CART_DICT[table_id][message['id']] = cartItem
-                elif message['action'] == 'delete':
-                    CART_DICT[table_id].pop(message['id'], None)
-                elif message['action'] == 'share':
-                    if message['id'] in CART_DICT[table_id]:
-                        CART_DICT[table_id][message['id']].addUserToItem(message['user'])
-                elif message['action'] == 'unshare':
-                    if message['id'] in CART_DICT[table_id]:
-                        CART_DICT[table_id][message['id']].removeUserFromItem(message['user'])
-                elif message['action'] == 'order':
-                    for cartItem in CART_DICT[table_id].values():
-                        if cartItem.get_orderedBy() == message['user']:
-                            cartItem.set_isOrdered_true()
+                table_id = message['table_id']
 
-                # print(json.dumps(list(CART_DICT[table_id].values())))
-                json_message = json.dumps(list(CART_DICT[table_id].values()), default=lambda o: o.__dict__, indent=4)
-                print(json_message)
-                await broadcast(json_message, table_id)
+
+                # Add user to CLIENT_TABLES if nonexistent
+                if not table_id in CLIENT_TABLES:
+                    CLIENT_TABLES[table_id] = []
+                if not websocket in CLIENT_TABLES[table_id]:
+                    print("adding websocket to client tables")
+                    
+                    CLIENT_TABLES[table_id].append(websocket)
+                    CLIENT_TABLEID_LOOKUP[websocket] = table_id
+
+                    print(CLIENT_TABLES)
+                    
+                # Send latest cart data if user goes to Menu screen from camera screen
+                if message['flag']:
+                    if table_id in CART_DICT:
+                        json_message = json.dumps(list(CART_DICT[table_id].values()), default=lambda o: o.__dict__, indent=4)
+                        await websocket.send(json_message)
+                # All other messages should be treated as edits to the cart
+                else:
+                    if not table_id in CART_DICT:
+                        CART_DICT[table_id] = dict()
+                    if message['action'] == 'add':
+                        cartItem = CartItem(message['id'], message['item'], message['user'])
+                        CART_DICT[table_id][message['id']] = cartItem
+                    elif message['action'] == 'delete':
+                        CART_DICT[table_id].pop(message['id'], None)
+                    elif message['action'] == 'share':
+                        if message['id'] in CART_DICT[table_id]:
+                            CART_DICT[table_id][message['id']].addUserToItem(message['user'])
+                    elif message['action'] == 'unshare':
+                        if message['id'] in CART_DICT[table_id]:
+                            CART_DICT[table_id][message['id']].removeUserFromItem(message['user'])
+                    elif message['action'] == 'order':
+                        for cartItem in CART_DICT[table_id].values():
+                            if cartItem.get_orderedBy() == message['user']:
+                                cartItem.set_isOrdered_true()
+
+                        json_message = json.dumps(list(CART_DICT[table_id].values()), default=lambda o: o.__dict__, indent=4)
+                        print(json_message)
+                        await broadcast_to_servers(json_message, table_id)
+
+                    json_message = json.dumps(list(CART_DICT[table_id].values()), default=lambda o: o.__dict__, indent=4)
+                    print(json_message)
+                    await broadcast_to_customers(json_message, table_id)
+
+            # Message from a Server
+            if 'restaurant' in message:
+                # Add user to SERVER_TABLES if nonexistent
+                for table_id in message['table_id_list']:
+                    if not table_id in SERVER_TABLES:
+                        SERVER_TABLES[table_id] = []
+                    if not websocket in SERVER_TABLES[table_id]:
+                        print("adding websocket to SERVER tables")
+                        SERVER_TABLES[table_id].append(websocket)
+
+                    # Add table to SERVER_TABLE_LOOKUP if nonexistent
+                    if not websocket in SERVER_TABLE_LOOKUP:
+                        SERVER_TABLE_LOOKUP[websocket] = []
+                    if not table_id in SERVER_TABLE_LOOKUP[websocket]:
+                        SERVER_TABLE_LOOKUP[websocket].append(table_id)
+
+                if message['restaurant']: # Joining websocket
+                    # Send data to server
+                    json_message= []
+                    for table_id in SERVER_TABLE_LOOKUP[websocket]:
+                        json_message.append(json.dumps(list(CART_DICT[table_id].values()), default=lambda o: o.__dict__, indent=4))
+                    await websocket.send(json_message)
+                else: # Modify status of order
+                    print("hello!")
+                    
                 
     except Exception as e:
         print("an error occurred")
@@ -97,10 +130,19 @@ async def handler(websocket):
         #     del CLIENT_TABLES[table_id]
         #     print(CLIENT_TABLES)
 
-async def broadcast(message, table_id):
-    print('broadcasting')
+async def broadcast_to_customers(message, table_id):
+    print('broadcasting to customers of ' + str(table_id))
 
     for websocket in CLIENT_TABLES[table_id].copy():
+        try:
+            await websocket.send(message)
+        except websockets.ConnectionClosed:
+            pass
+        
+async def broadcast_to_servers(message, table_id):
+    print('broadcasting to servers of ' + str(table_id))
+
+    for websocket in SERVER_TABLES[table_id].copy():
         try:
             await websocket.send(message)
         except websockets.ConnectionClosed:
