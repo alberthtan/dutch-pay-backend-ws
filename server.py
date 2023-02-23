464f6b6fe4832fa6b7a34757bfa3e782e3ccfce7
Allen Chun <allenchun@Allens-MacBook-Pro.local>
2023-02-23 17:46:52 -0500

    test

--- server.py
+++ server.py
@@ -134,7 +134,7 @@ async def handler(websocket):
                             await client.send(json.dumps(message))
 
                         # Update servers with new set of tables (after deleting)
-                        for server in SERVER_TABLES[table_id]:
+                        for server in SERVER_TABLES[table_id].copy():
                             print("clearing server")
                             json_message = []
                             for id in SERVER_TABLE_LOOKUP[server]:
