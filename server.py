6340906cc28f2f5ed1d860e099c7e85ff1f711cc
Allen Chun <allenchun@Allens-MacBook-Pro.local>
2023-02-22 22:21:35 -0500

    test

--- server.py
+++ server.py
@@ -98,13 +98,14 @@ async def handler(websocket):
                             # print("UYODFUOUFOISDJFIOLJDFOIFJIOSDJFOSiJFF")
                             # print(CART_DICT[table_id].values())
                             # json_message = list(CART_DICT[table_id].values()), default=lambda o: o.__dict__, indent=4
-                            json_message = json.dumps(list(CART_DICT[table_id].values()), default=lambda o: o.__dict__, indent=4)
+                            json_message.append(json.dumps(list(CART_DICT[table_id].values()), default=lambda o: o.__dict__, indent=4))
                             # json_message.append(json.dumps(list(CART_DICT[table_id].values()), default=lambda o: o.__dict__, indent=4))
-                            message = {
-                                "json_message": json_message,
-                                "table_id": table_id
-                            }
-                            await websocket.send(json.dumps(message))
+                    message = {
+                        "json_message": json.dumps(json_message),
+                        "table_id": table_id,
+                        "refresh": True
+                    }
+                    await websocket.send(json.dumps(message))
                 else: # Modify status of order
                     print("hello!")
                     
@@ -156,7 +157,8 @@ async def broadcast_to_servers(json_message, table_id):
         try:
             message = {
                 "json_message": json_message,
-                "table_id": table_id
+                "table_id": table_id,
+                "refresh": False
             }
             await websocket.send(json.dumps(message))
         except websockets.ConnectionClosed:
