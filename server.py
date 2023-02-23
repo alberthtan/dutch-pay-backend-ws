2425ebf3a3dccc9b7e8d0ef6eed8bb8edc1f68a0
Allen Chun <allenchun@Allens-MacBook-Pro.local>
2023-02-23 14:46:23 -0500

    test

--- server.py
+++ server.py
@@ -39,6 +39,8 @@ async def handler(websocket):
                     CLIENT_TABLEID_LOOKUP[websocket] = table_id
 
                     print(CLIENT_TABLES)
+                print("here 0")
+                print(message['flag'])
                     
                 # Send latest cart data if user goes to Menu screen from camera screen
                 if message['flag']:
@@ -47,6 +49,8 @@ async def handler(websocket):
                         await websocket.send(json_message)
                 # All other messages should be treated as edits to the cart
                 else:
+                    print("here 1")
+                    print(message['action'])
                     if not table_id in CART_DICT:
                         CART_DICT[table_id] = dict()
                     if message['action'] == 'add':
@@ -62,7 +66,7 @@ async def handler(websocket):
                             CART_DICT[table_id][message['id']].removeUserFromItem(message['user'])
                     elif message['action'] == 'order':
                         for cartItem in CART_DICT[table_id].values():
-                            if cartItem.get_orderedBy() == message['user'] and cartItem.get_status == "pending":
+                            if cartItem.get_orderedBy() == message['user']:
                                     cartItem.set_status("ordered")
                                     print("HEREEEE  ")
                                     print(cartItem)
